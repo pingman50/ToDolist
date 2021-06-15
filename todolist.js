abad6c99c1fbc2d8227866640d50c5abbd26aa96
Ilgıt Yıldırım <ilgit.yildirim@triplebits.com>
2021-06-15 14:07:20 +0200

    small changes

--- todolist.js
+++ todolist.js
@@ -1,21 +1,63 @@
-let addToDoButton = document.getElementById("addToDo");
-let toDoContainer = document.getElementById("toDoContainer");
-let inputField= document.getElementById("inputField"); 
-addToDoButton.addEventListener('click', function(){
-    var paragraph = document.createElement('p')
-    paragraph.classList.add('paragraph-styling');
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = "";
-    paragraph.addEventListener('click' , function(){
-        paragraph.style.textDecoration = "line-through"
-        })
-        paragraph.addEventListener('dblclick' , function(){
-            toDoContainer.removeChild(paragraph);
+const addToDoButton = document.getElementById('addToDo');
+const toDoContainer = document.getElementById('toDoContainer');
+const inputField = document.getElementById('inputField');
 
-    })
+const Storage = localStorage;
+const storageKey = 'todos';
 
-})
+const getItems = () => {
+  const items = Storage.getItem(storageKey);
+  if (!items) {
+    return null;
+  }
+  return JSON.parse(items);
+};
+
+const items = getItems();
+
+const addToStorage = (item) => {
+  let _items = items;
+  if (!_items) {
+    _items = [];
+  }
+  _items.push(item);
+  Storage.setItem(storageKey, JSON.stringify(_items));
+};
+
+const addListItem = (item) => {
+  const paragraph = document.createElement('p');
+  paragraph.classList.add('paragraph-styling');
+  paragraph.innerText = item;
+  toDoContainer.appendChild(paragraph);
+
+  paragraph.addEventListener('click', function () {
+    paragraph.style.textDecoration = 'line-through';
+  });
+
+  paragraph.addEventListener('dblclick', function () {
+    toDoContainer.removeChild(paragraph);
+    const index = (getItems() || []).findIndex(_item => _item === item);
+    let _items = items;
+    if (index < 0) {
+      return;
+    }
+    _items.splice(index, 1);
+    Storage.setItem(storageKey, JSON.stringify(_items));
+  });
+};
+
+if (items) {
+  items.map(item => {
+    addListItem(item);
+  });
+}
+
+addToDoButton.addEventListener('click', function () {
+  const item = inputField.value;
+  addToStorage(item);
+  addListItem(item);
+  inputField.value = '';
+});
 
 
 
